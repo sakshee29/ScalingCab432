d17a60668c2265625c7ae99398acdb04ba687865
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-11-13 00:37:29 +1000

    classification UI working

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,22 @@
-var express = require('express');
+var express = require("express");
 var axios = require("axios");
-const mobilenet = require('@tensorflow-models/mobilenet');
+const mobilenet = require("@tensorflow-models/mobilenet");
 
 const tf = require("@tensorflow/tfjs");
 const tfcore = require("@tensorflow/tfjs-node");
 
 const image = require("get-image-data");
-const { response } = require('express');
-
+require('dotenv').config();
 
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("index");
 });
 
 const imageAPI = {
-  api_key: "563492ad6f917000010000014e2e8fed9d1d45b2b612e94a91a62e94",
+  api_key: process.env.EXPRESS_APP_IMAGE_API_KEY,
 };
 
 function createAPIOptions(query) {
@@ -30,46 +29,61 @@ function createAPIOptions(query) {
   return options;
 }
 
-
-
-router.get('/classify', async function(req, res, next) {
-
-  const options = createAPIOptions(req.query.query);
+router.get("/classify", async function (req, res, next) {
+  let searchTerm = req.query.query;
+  const options = createAPIOptions(searchTerm);
   const url = `https://${options.hostname}${options.path}`;
-  let config = {'Authorization': `${imageAPI.api_key}`};
+  let config = { Authorization: `${imageAPI.api_key}` };
 
   await axios
-    .get(url, {headers : config})
+    .get(url, { headers: config })
     .then((response) => {
-      console.log(response.status);
       return response.data;
     })
     .then((data) => {
       let resultJson = data;
-      /* If the query is garbage i.e no photos exist */
-      if(resultJson.total_results > 0){
-        res.status(200).send({
-          result: resultJson,
+
+      if (resultJson.total_results > 0) {
+        let photoUrl = resultJson.photos[0].src.original;
+        let displayMessage = `You searched for ${searchTerm}`;
+        // console.log(photoUrl);
+
+        classifyImage(photoUrl)
+        .then((imageClassification) => {
+          // console.log(imageClassification[0].className);
+          res.render("classify", { displayMessage, photoUrl, imageClassification});
+          // res.status(200).send({
+          //   classification: imageClassification,
+          // });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send("Something went wrong while fetching image from URL.");
         });
+      } 
+
+      /* If the query is garbage i.e no photos exist */
+      else {
+        let displayMessage = "Please enter a valid query";
+        res.render("classify" , { displayMessage });
       }
-      else{
-        res.status(200).send("Please enter a valid query");
-      }
-      
     })
     .catch((err) => {
-      let status = 500;
+      let status = err.status;
       let errMessage = err.message;
 
-      if (err.response) {
-        /*If query is empty */
-        status = err.response.data.status;
-        errMessage = err.response.data.code; 
-        // console.log(err.response.data);
-      }
-      res.status(status).send(errMessage);
-    })
-
+      // if (err.response) {
+      //   /*If query is empty */
+      //   status = err.response.data.status;
+      //   errMessage = err.response.data.code;
+      //   console.log(err.response.data);
+      //   // res.render("classify" , { displayMessage:errMessage });
+      // }
+      
+      res.status(500).render("error", {errMessage, status});
+      // res.status(status).send(errMessage);
+      // res.status(500).send(err);
+    });
 
   // whatIsThis(req.query.query)
   //   .then((imageClassification) => {
@@ -86,7 +100,7 @@ router.get('/classify', async function(req, res, next) {
   //   });
 });
 
-function whatIsThis(url) {
+function classifyImage(url) {
   return new Promise((resolve, reject) => {
     image(url, async (err, image) => {
       if (err) {
