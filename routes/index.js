41822043be89b0d0ca8a7a3e6f9f836700f25621
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-11-13 04:40:05 +1000

    Persistence working

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
-var express = require("express");
-var axios = require("axios");
+const express = require("express");
+const axios = require("axios");
 const mobilenet = require("@tensorflow-models/mobilenet");
-
 const tf = require("@tensorflow/tfjs");
 const tfcore = require("@tensorflow/tfjs-node");
-
 const image = require("get-image-data");
-require('dotenv').config();
+const redis = require("redis");
 
 var router = express.Router();
+
+require("dotenv").config();
+const AWS = require("aws-sdk");
+
+/* ------------------------ Cloud Services Set-up  ------------------------ */
+/* Create unique bucket name (S3) */
+const bucketName = "a2-sak-test";
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+s3.createBucket({ Bucket: bucketName })
+  .promise()
+  .then(() => console.log(`Created bucket: ${bucketName}`))
+  .catch((err) => {
+    // Ignore 409 errors which indicate that the bucket already exists
+    if (err.statusCode !== 409) {
+      console.log(`Error creating bucket: ${err}`);
+    }
+  });
+
+/* Redis setup */
+const redisClient = redis.createClient();
+redisClient.connect().catch((err) => {
+  console.log(err);
+});
+
+/* ------------------------ Routes ------------------------ */
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -32,36 +56,96 @@ router.get("/classify", async function (req, res, next) {
 
       if (resultJson.total_results > 0) {
         let photoUrl = resultJson.photos[0].src.original;
-        let displayMessage = `You searched for ${searchTerm}`;
         // console.log(photoUrl);
+        let displayMessage = `You searched for ${searchTerm}`;
+        const persistenceKey = `${photoUrl}`;
 
-        classifyImage(photoUrl)
-        .then((imageClassification) => {
+        redisClient.get(persistenceKey).then((result) => {
+          if (result) {
+            /* Serve from Redis */
+            const resultJSON = JSON.parse(result);
+            res.json({ source: "Redis Cache", ...resultJSON });
+          } 
+          else {
+            /* Check if it's in S3 */
+            const params = { Bucket: bucketName, Key: persistenceKey };
+            s3.getObject(params)
+              .promise()
+              .then((result) => {
+                /* Serve from S3 and store in redis */
+                const resultJSON = JSON.parse(result.Body);
+                res.json({ source: "S3 Bucket", ...resultJSON });
 
-          console.log(imageClassification);
-          let prob = ((imageClassification[0].probability)*100).toFixed(2);
-          res.render("classify", { displayMessage, photoUrl, prob, imageClassification});
-          // res.status(200).send({
-          //   classification: imageClassification,
-          // });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).send("Something went wrong while fetching image from URL.");
+                resultJSON.source = "Redis Cache";
+                redisClient.setEx(persistenceKey, 3600, JSON.stringify(resultJSON));
+              })
+              .catch((err) => {
+                if (err.statusCode === 404) {
+                  /* Clasify the Image and store in Redis and S3 */
+                  classifyImage(photoUrl)
+                    .then((imageClassification) => {
+                      const responseJSON = imageClassification;
+
+                      /* Store in S3 Bucket*/
+                      const body = JSON.stringify(responseJSON);
+                      const objectParams = {
+                        Bucket: bucketName,
+                        Key: persistenceKey,
+                        Body: body,
+                      };
+
+                      /* Store in Redis*/       
+                      redisClient.setEx(
+                        persistenceKey,
+                        3600,
+                        JSON.stringify({ source: "Redis Cache", ...responseJSON })
+                      );
+
+                      s3.putObject(objectParams)
+                      .promise()
+                      .then(() => {
+                        console.log(
+                          `Successfully uploaded data to ${bucketName}/${persistenceKey}`
+                        ); 
+                      });
+
+                      console.log(imageClassification);
+                      let prob = (
+                        imageClassification[0].probability * 100
+                      ).toFixed(2);
+                      res.render("classify", {
+                        displayMessage,
+                        photoUrl,
+                        prob,
+                        imageClassification,
+                      });
+                      // res.status(200).send({
+                      //   classification: imageClassification,
+                      // });
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                      res
+                        .status(500)
+                        .send(
+                          "Something went wrong while fetching image from URL."
+                        );
+                    });
+                }
+              });
+          }
         });
-      } 
-
-      /* If the query is garbage i.e no photos exist */
-      else {
+      } else {
+        /* If the query is garbage i.e no photos exist */
         let displayMessage = `Please enter a valid query, I don't know what ${searchTerm} means`;
-        res.render("classify" , { displayMessage });
+        res.render("classify", { displayMessage });
       }
     })
     .catch((err) => {
       let status = err.status;
       let errMessage = err.message;
 
-      res.status(500).render("error", {errMessage, status});
+      res.status(500).render("error", { errMessage, status });
     });
 });
 
